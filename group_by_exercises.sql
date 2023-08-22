c0f27704a02d7820667ca3b16b331103acbd6edd
Damian Solis <damianrsolis@gmail.com>
2023-08-22 09:27:03 -0500

    ccorrected from the walkthrough

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -2,23 +2,32 @@ USE employees;
 
 SHOW DATABASES;
 
-SELECT *
+SELECT DISTINCT title
 From titles;
 
 SELECT last_name
 FROM employees
 WHERE last_name LIKE 'E%E'
-GROUP BY last_name;
+GROUP BY last_name
+Order BY last_name;
+
+
 
 SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE 'E%E'
-GROUP BY first_name, last_name;
+GROUP BY first_name, last_name
+ORDER BY last_name;
 
 
-#  you need to fix this one
-SELECT COUNT(*),
-       last_name
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+  And last_name NOT LIKE '%qu%'
+GROUP BY last_name; #group by is needed for the 3 names only to appear as the exercise.
+
+SELECT COUNT(*) AS total, last_name AS "last Name"           #added count
 FROM employees
 WHERE last_name LIKE '%q%'
   And last_name NOT LIKE '%qu%'
